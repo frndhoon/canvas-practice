4ec29f5e482caf99870eec0384cd0a4a573ab14f
frndhoon <frndhoon@gmail.com>
2025-01-09 15:04:55 +0900

    chore: rename app to 'grim-talk

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { DrawingCanvas } from './components/DrawingCanvas';
-import './App.css';
 
 function App() {
   return (
     <div className='min-h-screen bg-gray-100 py-6 flex flex-col justify-center items-center'>
       <div className='max-w-4xl w-full mx-auto px-4'>
-        <h1 className='text-4xl font-bold text-center text-gray-800 mb-8'>그림 그리기 앱</h1>
+        <div className='flex flex-col items-center mb-8'>
+          <h1 className='text-4xl font-bold text-center text-gray-800'>그림톡</h1>
+          <p className='text-xl font-bold text-center text-gray-800'>grim-talk</p>
+        </div>
         <div className='bg-white rounded-lg shadow-lg p-6'>
           <DrawingCanvas />
         </div>
